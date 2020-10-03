bb20360e985eb7a8a478a872e53d703728c1c503
spicesouls <59148900+spicesouls@users.noreply.github.com>
2020-10-03 14:09:58 +0100

    Update add-to-bin.sh

--- add-to-bin.sh
+++ add-to-bin.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-sudo cp spicescript2.py /usr/local/bin/spicescript2
-sudo chmod a+x /usr/local/bin/spicescript2
+
+cp spicescript2.py /usr/local/bin/spicescript2
+chmod a+x /usr/local/bin/spicescript2
+chmod u+s /usr/local/bin/spicescript2
